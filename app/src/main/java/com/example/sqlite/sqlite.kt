e7b4e09079c1ece3c447de1ff7849b2c6ae778bc
JPArenas <jpinedaarenas@danielcastelao.org>
2025-12-12 11:39:23 +0100

    SQLITE PMDM

--- app/src/main/java/com/example/sqlite/sqlite.kt
+++ app/src/main/java/com/example/sqlite/sqlite.kt
@@ -1,7 +1,6 @@
 package com.example.sqlite
 
 import android.provider.BaseColumns
-import FeedEntry;
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
